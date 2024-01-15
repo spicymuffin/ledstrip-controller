a9c72dc6688ae761e32b95be00aac98b9d8d5b43
spicymuffin <luigicussigh59@gmail.com>
2024-01-15 23:32:00 +0300

    working controller

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -18,13 +18,85 @@ SEND_SOCKET = None
 def startup():
     global SEND_SOCKET
     SEND_SOCKET = socket(AF_INET, SOCK_DGRAM)
+    mainloop()
 
 
 def mainloop():
     global SEND_SOCKET
     while True:
-        command = input()
-        SEND_SOCKET.sendto(command.encode("utf-8"), (SERVER_IP, PORT_NUMBER))
+        inp = input()
+        cmd, payload = interpret(inp)
+        if payload == -1:
+            print(f"'{cmd}' command unrecognized....")
+        else:
+            SEND_SOCKET.sendto(payload.encode("utf-8"), (SERVER_IP, PORT_NUMBER))
+            data, (ip, port) = SEND_SOCKET.recvfrom(1024)
+            print(data)
+
+
+def plgenerator_change_animation(args):
+    return "change_animation" + (" " + " ".join(args)) if len(args) != 0 else ""
+
+
+def plgenerator_ping(args):
+    pass
+
+
+def plgenerator_off():
+    return "off"
+
+
+command_dictionary = {
+    "change_animation": {
+        "plgenerator": plgenerator_change_animation,
+        "aliases": [
+            "change_animation",
+            "changeanimation",
+            "change_anim",
+            "changeanim",
+            "ca",
+        ],
+    },
+    "ping": {
+        "plgenerator": plgenerator_ping,
+        "aliases": [
+            "ping",
+            "p",
+        ],
+    },
+    "off": {
+        "plgenerator": plgenerator_off,
+        "aliases": [
+            "off",
+            "o",
+        ],
+    },
+}
+
+
+def cmd_argument_interpreter(command_argument):
+    global command_dictionary
+
+    for i in command_dictionary.keys():
+        if command_argument in command_dictionary[i]["aliases"]:
+            return i
+    return -1
+
+
+def interpret(input):
+    splt = input.split()
+    cmd = splt[0]
+    args = splt[1:]
+
+    cmd_intepret_result = cmd_argument_interpreter(command_argument=cmd)
+
+    if cmd_intepret_result == -1:
+        return (cmd, -1)
+
+    # generate payload
+    payload = command_dictionary[cmd_intepret_result]["plgenerator"](args)
+
+    return (cmd, payload)
 
 
 if __name__ == "__main__":
